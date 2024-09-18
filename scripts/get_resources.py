e44d89683131211410a52c0211ed2d34b3522b50
marccambon <marccambonm@gmail.com>
2024-09-18 14:31:00 +0200

    add cpu+ram+disk

--- scripts/get_resources.py
+++ scripts/get_resources.py
@@ -1,9 +1,9 @@
 import psutil
 
 #CPU usage
-CPU_usage=psutil.cpu_percent()
+CPU_usage=psutil.cpu_percent(interval=1)
 
-print(CPU_usage)
+print(CPU_usage,"%")
 
 #RAM usage
 RAM_used=psutil.virtual_memory()[2]
@@ -11,3 +11,16 @@ RAM_used_GB=psutil.virtual_memory()[3]/1000000000
 
 print(RAM_used,"%")
 print(RAM_used_GB,"GB") 
+
+#Disk usage
+disk_command=psutil.disk_usage("/")
+
+disk_total= disk_command.total
+disk_used= disk_command.free
+disk_free= disk_command.used
+
+print(disk_total)
+print(disk_used)
+print(disk_free)
+
+#Network usage
