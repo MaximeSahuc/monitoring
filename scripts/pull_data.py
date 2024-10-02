ebbc1dbc81545de85608630e3a4bd1aeb1c69544
marccambon <marccambonm@gmail.com>
2024-10-02 10:43:36 +0200

    error handling

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -5,5 +5,11 @@ try:
     response_API = requests.get('http://127.0.0.1:8081/status')
     response_API.raise_for_status()
     print(response_API.json())
-except requests.exceptions.HTTPError as err:
-    raise SystemExit(err)
+except requests.exceptions.RequestException as errr:
+    print ("OOps: Something Else",errr)
+except requests.exceptions.HTTPError as errh:
+    print ("Http Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)
