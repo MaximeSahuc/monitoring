49d558baed005cfc4541b28dd4652e1dd8a8689f
marccambon <marccambonm@gmail.com>
2024-10-03 13:52:49 +0200

    add data to db fonction

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -2,23 +2,6 @@ import requests
 import json
 import mariadb
 
-try:
-    response_API = requests.get('http://127.0.0.1:8081/status')
-    response_API.raise_for_status()
-    print(response_API.json())
-except requests.exceptions.RequestException as errr:
-    print ("OOps: Something Else",errr)
-except requests.exceptions.HTTPError as errh:
-    print ("Http Error:",errh)
-except requests.exceptions.ConnectionError as errc:
-    print ("Error Connecting:",errc)
-except requests.exceptions.Timeout as errt:
-    print ("Timeout Error:",errt)
-
-
-
-data=json.load(response_API)
-print(data)
 
 def connect_db():
     try:
@@ -33,4 +16,41 @@ def connect_db():
 
 
 def add_data():
-    
+    try:
+        cursor=mariadb.cursor()
+        statement="INSERT INTO <table> (cpu, ramGB, disk_used, disk_write_speed, disk_read_speed, dl_speed, ul_speed) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (
+            data["cpu"],
+            data["ramGB"],
+            data["disk_used"],
+            data["disk_write_speed"],
+            data["disk_read_speed"],
+            data["dl_speed"],
+            data["ul_speed"]
+        )
+        cursor.execute(statement,values)
+        mariadb.commit()
+        print("Successfully added entry to database")
+    except mariadb.Error as erradddb:
+        print("Error adding entry to database", erradddb)
+
+
+
+try:
+    response_API = requests.get('http://127.0.0.1:8081/status')
+    response_API.raise_for_status()
+    print(response_API.json())
+except requests.exceptions.RequestException as errr:
+    print ("OOps: Something Else",errr)
+except requests.exceptions.HTTPError as errh:
+    print ("Http Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)
+
+
+data=json.load(response_API)
+connect_db()
+add_data(data)
+
