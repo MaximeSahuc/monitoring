70421f80e6060fb0d88dd88a285d88fb120b6e38
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-10-09 19:01:57 +0200

    .

--- docker/pyagent/pull_data.py
+++ docker/pyagent/pull_data.py
@@ -1,47 +1,78 @@
 import requests
 import mariadb
 
+DB_HOST = "db"
+DB_PORT = 3306
+DB_USER = "root"
+DB_PASS = "root"
+DATABASE_NAME = "monitoring"
 
-def connect_db():
+def connect_db(database=None):
     try:
-        conn=mariadb.connect(
-            host="127.0.0.1",
-            port=3306,
-            user="root",
-            password="root",
-            database="monitoring"
-        )
-        return conn
+        if database:
+            return mariadb.connect(
+                host = DB_HOST,
+                port = DB_PORT,
+                user = DB_USER,
+                password = DB_PASS,
+                database = DATABASE_NAME
+            )
+        else:
+            return mariadb.connect(
+                host = DB_HOST,
+                port = DB_PORT,
+                user = DB_USER,
+                password = DB_PASS,
+            )
     except mariadb.Error as errdb:
         print("Error connecting to the database",errdb)
 
 
 def check_db():
-    conn=connect_db()
-    try:
-        with conn.cursor() as cursor:
-            statement="""CREATE TABLE IF NOT EXISTS `data_webserv` (
-    `cpu` float NOT NULL,
-    `ramGB` float NOT NULL,
-    `disk_used` float NOT NULL,
-    `disk_write_speed` float NOT NULL,
-    `disk_read_speed` float NOT NULL,
-    `dl_speed` float NOT NULL,
-    `ul_speed` float NOT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;"""
-        cursor.execute(statement)
-        conn.commit()
-    except mariadb.Error as errcrdb:
-            print("Error creating table",errcrdb)
-    finally:
+    conn = connect_db()
+
+    #  Create database
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS `monitoring` COLLATE 'utf8mb4_general_ci';")
+            conn.commit()
+        except Exception as err:
+            print(err)
+            exit()
+        finally:
+            conn.close()
+    
+    #  Create table
+    conn = connect_db(database=DATABASE_NAME)
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS `data_webserv` (
+                `cpu` float NOT NULL,
+                `ramGB` float NOT NULL,
+                `disk_used` float NOT NULL,
+                `disk_write_speed` float NOT NULL,
+                `disk_read_speed` float NOT NULL,
+                `dl_speed` float NOT NULL,
+                `ul_speed` float NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4  COLLATE 'utf8mb4_general_ci';
+                """
+            )
+            conn.commit()
+        except Exception as err:
+            error = True
+            print(err)
+            exit()
+        finally:
             conn.close()
 
 
 def add_data(data):
-    conn=connect_db()
+    conn = connect_db(database=DATABASE_NAME)
     try:
         with conn.cursor() as cursor:
-            statement="INSERT INTO data_webserv (cpu, ramGB, disk_used, disk_write_speed, disk_read_speed, dl_speed, ul_speed) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            statement = "INSERT INTO data_webserv (cpu, ramGB, disk_used, disk_write_speed, disk_read_speed, dl_speed, ul_speed) VALUES (%s, %s, %s, %s, %s, %s, %s)"
             values = (
                 data.get("cpu"),
                 data.get("ramGB"),
@@ -51,11 +82,12 @@ def add_data(data):
                 data.get("dl_speed"),
                 data.get("ul_speed")
             )
-            cursor.execute(statement,values)
+            cursor.execute(statement, values)
             conn.commit()
             print("Successfully added entry to database")
     except mariadb.Error as erradddb:
         print("Error adding entry to database", erradddb)
+        exit()
     finally:
         conn.close()
 
@@ -74,12 +106,25 @@ def request_api(url):
         print ("Timeout Error:",errt)
 
 
-url='http://127.0.0.1:8081/status'
+def main():
+    from time import sleep
+    web_server_url='http://web-server:8081/status'
+    sleep_time = 1 * 60  # 1 minute
 
-api_data=request_api(url)
+    print("Starting monitoring agent")
 
-if api_data:
     check_db()
-    add_data(api_data)
+
+    while True:
+        api_data=request_api(web_server_url)
+
+        if api_data:
+            add_data(api_data)
+        else:
+            print("Error: no data received from server !")
+        
+        sleep(sleep_time)
 
 
+if __name__ == "__main__":
+    main()
