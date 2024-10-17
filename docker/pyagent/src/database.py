3a12d0410036e3502665a600bb1f7b61c5ea653d
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-10-17 13:25:45 +0200

    apache agent

--- docker/pyagent/src/database.py
+++ docker/pyagent/src/database.py
@@ -1,8 +1,12 @@
 
 class Database:
 
-    def __init__(self, host):
+    def __init__(self, host, user, password, port=3306):
         self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
     
     def print_host(self):
         print(self.host)
--- docker/pyagent/src/main.py
+++ docker/pyagent/src/main.py
@@ -1,5 +1,6 @@
 from database import Database
+import config as cfg
 
-db = Database(host="aaa")
+db = Database(host=cfg.DB_HOST, user=cgf.user, password=cgf.password)
 
 db.print_host()
